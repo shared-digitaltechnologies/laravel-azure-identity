f9b7ee8405b32104fec2ec90954948a472ce91bf
roelhem <ik@roelweb.com>
2024-03-12 14:00:31 +0100

    Sync with monorepo till f45ea19b088eb90ee9d5034375247debb6da5122

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,8 @@ namespace Shrd\Laravel\Azure\Identity;
 
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Shrd\Laravel\Azure\Identity\Certificates\SimpleClientCertificateFactory;
+use Shrd\Laravel\Azure\Identity\Contracts\ClientCertificateFactory;
 use Shrd\Laravel\Azure\Identity\Contracts\TokenCredential;
 use Shrd\Laravel\Azure\Identity\Contracts\TokenCredentialDriver;
 use Shrd\Laravel\Azure\Identity\Contracts\TokenCredentialFactory;
@@ -13,6 +15,9 @@ class ServiceProvider extends BaseServiceProvider {
 
     public function register(): void
     {
+        $this->app->singleton(SimpleClientCertificateFactory::class);
+        $this->app->bind(ClientCertificateFactory::class, SimpleClientCertificateFactory::class);
+
         $this->app->singleton(TokenCredentialManager::class);
         $this->app->bind(TokenCredentialFactory::class, TokenCredentialManager::class);
 
